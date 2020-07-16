4955302ae4c0492a6fd61226449defaf31f89d8e
berryboylb <phemmynesce4life@gmail.com>
2020-07-16 17:13:06 +0100

    new db file

--- dbconnect.php
+++ dbconnect.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "berryboy";
+$servername = "us-cdbr-east-02.cleardb.com";
+$username = "b7882e0919aa39";
+$password = "0d909835";
+$database = "heroku_0b8b343f558c684";
 
 // Create connection
 $con = mysqli_connect($servername, $username, $password, $database);
